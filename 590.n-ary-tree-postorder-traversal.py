da8221d2fcaa0dac7a199ed298ca9dc5c346495d
felipe-giusti <felipe.f.giusti@gmail.com>
2024-02-16 18:00:07 +0100

    590 postorder iteratively - first solve

---
+++ 590.n-ary-tree-postorder-traversal.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=590 lang=python3
+#
+# [590] N-ary Tree Postorder Traversal
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+from collections import deque
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        # post order -> left, right, root
+
+        stack = deque([(root, False)])
+        res = []
+
+        while stack:
+            cur, visited = stack.pop()
+
+            if cur is None:
+                continue
+
+            if visited or cur.children is None:
+                res.append(cur.val)
+            else:
+
+                stack.append((cur, True))
+                for n in reversed(cur.children):
+                    stack.append((n, False))
+        return res
+
+# @lc code=end
+
