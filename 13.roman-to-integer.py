dc4c152880f22b1422946235c5d8162ebc3ec39f
felipe-giusti <felipe.f.giusti@gmail.com>
2024-02-18 13:27:55 +0100

    optimization after checcking other solutions

--- 13.roman-to-integer.py
+++ 13.roman-to-integer.py
@@ -10,30 +10,25 @@ class Solution:
         
         roman_dict = {
             'M': 1000,
-            'CM': 900,
             'D': 500,
-            'CD': 400,
-             'C': 100,
-            'XC': 90,
+            'C': 100,
             'L': 50,
-            'XL': 40,
             'X': 10,
-            'IX': 9,
             'V': 5,
-            'IV': 4,
             'I': 1
         }
 
         n = 0
-        while s:
-            try:
-                n += roman_dict[s[:2]]
-                s = s[2:]
-            
-            except KeyError:
-                n += roman_dict[s[:1]]
-                s = s[1:]
-        return n
+        
+        s = s.replace('IV', 'IIII')
+        s = s.replace('IX', 'VIIII')
+        s = s.replace('XL', 'XXXX')
+        s = s.replace('XC', 'LXXXX')
+        s = s.replace('CD', 'CCCC')
+        s = s.replace('CM', 'DCCCC')
 
+        for c in s:
+            n += roman_dict[c]
+        return n
   
 # @lc code=end
