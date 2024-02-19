785f37f34036753968924a3fe99e078bf01f97d1
felipe-giusti <felipe.f.giusti@gmail.com>
2024-02-19 16:34:46 +0100

    15 good perfoemance

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -8,27 +8,42 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
-        # idea 2 -> sort + check sum
-
         triplets = set()
-        nums = sorted(nums)
+        
+        # separate negative, zero, positive
+        n, num_z, p = [], 0, []
+        for num in nums:
+            if num < 0:
+                n.append(num)
+            elif num == 0:
+                num_z += 1
+            else: p.append(num)
+        
+        # if three zeros, add zero
+        if num_z >= 3:
+            triplets.add((0, 0, 0))
 
-        for i in range(len(nums) - 2):
-            target = -nums[i]
-            #two sum
-            l = i+1
-            r = len(nums) - 1
+        ns, ps = set(n), set(p)
+        # if at least one zero, check n / p
+        if num_z:
+            for num in ps:
+                if -num in ns:
+                    triplets.add((-num, 0, num))
+        
+        def check_list(l: list, s: set):
+            for i in range(len(l) - 1):
+                for j in range(i+1, len(l)):
+                    target = -(l[i] + l[j])
+                    if target in s:
+                        triplets.add(
+                            tuple(sorted([l[i], l[j], target])))
 
-            while l < r:
-                # print(f'i: {i}, l" {l}, r:{r}  || len: {len(nums)}')
-                if nums[l] + nums[r] == target:
-                    triplets.add((nums[i], nums[l], nums[r]))
-                    l += 1
-                    r -= 1
-                elif nums[l] + nums[r] < target:
-                    l += 1
-                else:
-                    r -= 1
+        # no zeros, check if pair of negatives in p
+        check_list(n, ps)
+
+        # no zeros, check if pair of positives in n
+        check_list(p, ns)
+
         return triplets
                     
 
