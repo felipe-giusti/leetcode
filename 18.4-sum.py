6120bbf26611f3c09bcdf7a318e972799c8625ab
felipe-giusti <felipe.f.giusti@gmail.com>
2024-02-22 15:32:24 +0100

    18 loop optimization

--- 18.4-sum.py
+++ 18.4-sum.py
@@ -12,7 +12,11 @@ class Solution:
         
         nums.sort()
         for i in range(len(nums)-3):
+            # if i > 0 and nums[i] == nums[i-1]:
+            #     continue
             for j in range(i+1, len(nums)-2):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
                 # print(f'i {i}, j {j} || len {len(nums)}')
                 left = j+1
                 right = len(nums)-1
