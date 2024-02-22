1603f72b0e344e142af073a6f55f701d5940ac07
felipe-giusti <felipe.f.giusti@gmail.com>
2024-02-22 15:12:15 +0100

    18 sorted pointer approach

---
+++ 18.4-sum.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=18 lang=python3
+#
+# [18] 4Sum
+#
+
+# @lc code=start
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        #pointer approach
+        res = set()
+        
+        nums.sort()
+        for i in range(len(nums)-3):
+            for j in range(i+1, len(nums)-2):
+                # print(f'i {i}, j {j} || len {len(nums)}')
+                left = j+1
+                right = len(nums)-1
+                while left < right:
+                    s = nums[i] + nums[j] + nums[left] + nums[right]
+                    if s < target:
+                        left += 1
+                    elif s > target:
+                        right -= 1
+                    else:
+                        res.add((nums[i], nums[j], nums[left], nums[right]))
+                        left += 1
+
+        return res
+
+
+# @lc code=end
+
