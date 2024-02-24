5f1ba7d4aed2896dc616e98a5085099eab1b9bdb
felipe-giusti <felipe.f.giusti@gmail.com>
2024-02-25 00:26:53 +0100

    28 substring is needle

--- 28.find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -7,6 +7,11 @@
 # @lc code=start
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        for i in range(len(haystack)):
+            if haystack[i: i+len(needle)] == needle:
+                return i
+        return -1
+
+
 # @lc code=end
 
