54814371916aa543a154351afd7a1ffe79431c84
felipe-giusti <felipe.f.giusti@gmail.com>
2024-03-12 22:31:00 +0100

    42 bfs-like

---
+++ 42.jump-game-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        res, max_dist, i_end = 0, 0, 0
+
+        # because we are already getting max dist every time, we don't need to worry about going back indexes
+        for i in range(len(nums)-1):
+            max_dist = max(max_dist, i + nums[i])
+
+            if i == i_end:
+                res += 1
+                i_end = max_dist
+        return res
