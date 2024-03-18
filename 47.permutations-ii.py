f317fc9bf6120119d85d0cd909ce514e63597e89
felipe-giusti <felipe.f.giusti@gmail.com>
2024-03-18 14:52:24 +0100

    47 set resets on backtrack

--- 47.permutations-ii.py
+++ 47.permutations-ii.py
@@ -1,19 +1,21 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         #backtracking
-        res = set()
+        res = []
         visited = [False] * len(nums)
         
         # not optimal because adds same permutation
         def backtrack(p):
             if len(p) == len(nums):
-                res.add(tuple(p))
+                res.append(p[:])
                 return
 
+            n_used = set()
             for i in range(len(nums)):
-                if not visited[i]:
+                if not visited[i] and nums[i] not in n_used:
                     visited[i] = True
                     p.append(nums[i])
+                    n_used.add(nums[i])
 
                     backtrack(p)
 
