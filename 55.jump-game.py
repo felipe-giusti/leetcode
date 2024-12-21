285e1be5bf7c0569cb2f142d273662c05ea4daa8
felipe-giusti <felipe.f.giusti@gmail.com>
2024-12-21 18:40:51 -0300

    55 readability improvement

--- 55.jump-game.py
+++ 55.jump-game.py
@@ -10,13 +10,12 @@ class Solution:
         # go through array, if current > reachable, change
         # after looking up other solutions: we CAN have reachable be 0, what can't happen is not being able to jump AFTER reaching a spot, with this we can ignore the return conditional
         reachable = 0
-        for i, num in enumerate(nums):
+        for num in nums:
             # can't continue
             if reachable < 0:
                 return False
             
-            if reachable < num:
-                reachable = num
+            reachable = max(reachable, num)
 
             reachable -= 1
 
