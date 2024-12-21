a4439f1f303449fd1838b83d3043547af5e5988b
felipe-giusti <felipe.f.giusti@gmail.com>
2024-12-21 18:35:53 -0300

    55 after checking other solutions

--- 55.jump-game.py
+++ 55.jump-game.py
@@ -8,21 +8,19 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         # go through array, if current > reachable, change
+        # after looking up other solutions: we CAN have reachable be 0, what can't happen is not being able to jump AFTER reaching a spot, with this we can ignore the return conditional
         reachable = 0
         for i, num in enumerate(nums):
+            # can't continue
+            if reachable < 0:
+                return False
+            
             if reachable < num:
                 reachable = num
-
-            # can't continue
-            if reachable <= 0:
-                return i == len(nums) - 1
 
             reachable -= 1
 
         return True
             
-
-
-        
 # @lc code=end
 
