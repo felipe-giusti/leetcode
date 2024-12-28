4d931491ae64750241886144367f74b23639bd72
felipe-giusti <felipe.f.giusti@gmail.com>
2024-12-28 22:41:00 +0100

    66 idea 2

--- 66.plus-one.py
+++ 66.plus-one.py
@@ -8,11 +8,21 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         # ideas: list to integer -> sum -> convert back
-        # recurrence with +1
+        # recurrence / iterate over list adding +1
 
-        n = len(digits)
-        number = sum([digits[i] * (10**(n-i-1)) for i in range(n)]) + 1
-        return [int(digit) for digit in str(number)]
+        for i in range(len(digits) - 1, -1, -1):
+            
+            # no carry, just return
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+
+            # carry
+            digits[i] = 0
+
+        # no return after loop -> still has carry
+        return [1] + digits
+        
 
 # @lc code=end
 
