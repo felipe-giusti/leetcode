bb0876f8922a149e0c4251e2d6c7a2457a56881b
felipe-giusti <felipe.f.giusti@gmail.com>
2024-12-31 22:52:25 +0100

    72 1D dp

--- 72.edit-distance.py
+++ 72.edit-distance.py
@@ -16,30 +16,30 @@ class Solution:
         # delete: i+1, j
         # replace: it changes to the correct one, so i+1, j+1
 
-        # 1. create 2D dp and base case
+        # 1d dp -> I only need i-1 and j-1, so I can save it in a prev variable
+
 
         m, n = len(word1), len(word2)
 
-        # word2
-        dp = [[float("inf")]*(n+1) for _ in range(m+1)]
-
-        for i in range(m+1):
-            dp[i][0] = i
-        for j in range(n+1):
-            dp[0][j] = j
+        dp = [i for i in range(n+1)]
         
         for i in range(1, m+1):
-            for j in range(1, n+1):
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
-                else:
-                    dp[i][j] = 1 + min(
-                        dp[i-1][j],
-                        dp[i][j-1],
-                        dp[i-1][j-1]
-                    )
+            # new row
+            prev_diagonal = dp[0]
+            dp[0] = i
 
-        return dp[-1][-1]
+            for j in range(1, n+1):
+                temp = dp[j]
+                if word1[i-1] == word2[j-1]:
+                    dp[j] = prev_diagonal
+                else:
+                    dp[j] = 1 + min(
+                        dp[j],
+                        dp[j-1],
+                        prev_diagonal
+                    )
+                prev_diagonal = temp
+        return dp[-1]
                 
 
 # @lc code=end
